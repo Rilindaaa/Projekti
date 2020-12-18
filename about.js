76e179c1645f2b9cff21d2641dbde511b7b931d0
Rilinda Xhaqkaj <xhaqkaj.r@gmail.com>
2020-12-18 23:55:19 +0100

    finale

--- about.js
+++ about.js
@@ -1,28 +1,24 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var Index = 1;
+showSlides(Index);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(Index += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(Index = n);
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName("Slides");
+  if (n > slides.length) {Index = 1}
+  if (n < 1) {Index = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[Index-1].style.display = "block";
+
 }
