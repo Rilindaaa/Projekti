366f53714d0a36bfc0b9f52ab0250fca04148b0d
Rilinda Xhaqkaj <xhaqkaj.r@gmail.com>
2021-03-10 21:45:55 +0100

    new

--- CONTENT/Kids.php
+++ CONTENT/Kids.php
@@ -3,12 +3,11 @@
   include '../components/header.php';
 ?>
         <main>
-            <div class="k-color">
             <div class="sector2">
                 <h1>K I D ' S E C T O R</h1>
             </div>
             <div id="k-txt">
-                <h1>Clothing is an important part of our everyday lives. It keeps us warm and protects us from the weather.</h1>
+                <p>Clothing is an important part of our everyday lives. It keeps us warm and protects us from the weather.</p>
             </div>
             <div id ="bxo">
                 <div class='kids'>
@@ -23,40 +22,121 @@
                     </div>
                 </div>
                 </div>
-                <h1 class="k-teksti">Do you want your kids to look amazing? <br>
-                You're in the right page.</h1>
+                <p class="k-teksti">Do you want your kids to look amazing? <br>
+                You're in the right page.</p>
                 <div class ="foto-txt">
                     <img id= "k-foto" src="../PICS/foto38.jpg">
-                    <h1>Dress your child in the right clothing for every activity they’re doing.</h1>
+                    <p>Dress your child in the right clothing for every activity they’re doing.</p>
                 </div>
-                <div id ="bxo">
-                    <div class='kids'>
-                        <div class='k-box'>
-                            <img src="../PICS/f50.jpg">                    
-                        </div>
-                        <div class='k-box'>
-                            <img src="../PICS/f39.jpg">                           
-                        </div>
-                        <div class='k-box'>
-                            <img src="../PICS/f51.jpg">           
-                        </div>
-                    </div>
-                    </div>
-                </div>
+
             </main>
-            <div id ="bxo">
-                <div class='kids'>
-                    <div class='k-box'>
-                        <img src="../PICS/foto36.png">                    
-                    </div>
-                    <div class='k-box'>
-                        <img src="../PICS/f40.jpg">                           
-                    </div>
-                    <div class='k-box'>
-                        <img src="../PICS/foto35.jpg">           
-                    </div>
+
+            <div class="shopK">
+                <h1>SHOP</h1>
+            </div>
+            <div class ="dressesK"> 
+                <h1>DRESSES</h1>
+            </div>
+        <div id ="boxes"> 
+            <div class='women'>
+                <div class='w-box'>
+                    <img src="../PICS/f94.jpg">
+                    <h3> LONG DRESS </h3>
+                    <b><p class = 'price'>PRICE: 60.00$</p></b>
+                          <button class="buynow"> BUY NOW </button>
                 </div>
+                <div class='w-box'>
+                    <img src="../PICS/f95.jpg">
+                    <h3> SHORT DRESS </h3>
+                   <b> <p class ='price'>PRICE: 25.99$</p></b>
+                        <button class="buynow"> BUY NOW </button>
                 </div>
-            <?php
+                <div class='w-box'>
+                    <img src="../PICS/f96.jpg">
+                    <h3> MID DRESS </h3>
+                   <b> <p class ='price'>PRICE: 45.99$</p></b>
+                        <button class="buynow"> BUY NOW </button>
+                </div>
+            </div>
+        </div>
+
+        <div class ="dressesK"> 
+                <h1>SHOES</h1>
+            </div>
+        <div id ="boxes"> 
+            <div class='women'>
+                <div class='w-box'>
+                    <img src="../PICS/f97.jpg">
+                    <h3> HEELS </h3>
+                    <b><p class = 'price'>PRICE: 20.00$</p></b>
+                          <button class="buynow"> BUY NOW </button>
+                </div>
+                <div class='w-box'>
+                    <img src="../PICS/f98.jpg">
+                    <h3>SNEAKERS</h3>
+                   <b> <p class ='price'>PRICE: 15.99$</p></b>
+                        <button class="buynow"> BUY NOW </button>
+                </div>
+                <div class='w-box'>
+                    <img src="../PICS/f99.jpg">
+                    <h3>SANDALS</h3>
+                   <b> <p class ='price'>PRICE: 10.99$</p></b>
+                        <button class="buynow"> BUY NOW </button>
+                </div>
+            </div>
+        </div>
+
+        <div class ="dressesK"> 
+                <h1>JEANS</h1>
+            </div>
+        <div id ="boxes"> 
+            <div class='women'>
+                <div class='w-box'>
+                    <img src="../PICS/f102.jpg">
+                    <h3> FORMAL JEANS </h3>
+                    <b><p class = 'price'>PRICE: 22.00$</p></b>
+                          <button class="buynow"> BUY NOW </button>
+                </div>
+                <div class='w-box'>
+                    <img src="../PICS/f101.jpg">
+                    <h3>CASUAL JEANS</h3>
+                   <b> <p class ='price'>PRICE: 12.99$</p></b>
+                        <button class="buynow"> BUY NOW </button>
+                </div>
+                <div class='w-box'>
+                    <img src="../PICS/f103.jpg">
+                    <h3>SPORT JEANS</h3>
+                   <b> <p class ='price'>PRICE: 10.00$</p></b>
+                        <button class="buynow"> BUY NOW </button>
+                </div>
+            </div>
+        </div>
+
+        <div class ="dressesK"> 
+                <h1>T-SHIRT</h1>
+            </div>
+        <div id ="boxes"> 
+            <div class='women'>
+                <div class='w-box'>
+                    <img src="../PICS/f104.jpg">
+                    <h3> FORMAL T-SHIRT </h3>
+                    <b><p class = 'price'>PRICE: 12.00$</p></b>
+                          <button class="buynow"> BUY NOW </button>
+                </div>
+                <div class='w-box'>
+                    <img src="../PICS/f107.jpg">
+                    <h3>CASUAL T-SHIRT</h3>
+                   <b> <p class ='price'>PRICE: 10.99$</p></b>
+                        <button class="buynow"> BUY NOW </button>
+                </div>
+                <div class='w-box'>
+                    <img src="../PICS/f106.jpg">
+                    <h3>SPORT T-SHIRT</h3>
+                   <b> <p class ='price'>PRICE: 8.00$</p></b>
+                        <button class="buynow"> BUY NOW </button>
+                </div>
+            </div>
+        </div>
+<?php
   include '../components/footer.php';
 ?>
